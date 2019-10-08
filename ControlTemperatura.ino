22a50e5daee30726d4613c60c0b60a9e6d35645d
juamarCas <42278423+juamarCas@users.noreply.github.com>
2019-10-08 09:36:46 -0500

    Update ControlTemperatura.ino

--- ControlTemperatura.ino
+++ ControlTemperatura.ino
@@ -5,41 +5,56 @@
 #define tempControl PD0
 #define humControl PD1
 #define batteryControl PB4
-
-//variables de la temperatura y humedad
 #define DHTPIN 7
 #define DHTTYPE DHT22
 int t, h; 
-
+int menuState = 0;
 int maxTemp = 32, minTemp = 29; 
 int maxHum, minHum;
 int counter = 0;
-int interval = 2000; 
-int lastTime; 
-int batteryLevel, minBatteryLevel; 
+const int interval = 2000; 
+int lastTime = 0; 
+int batteryLevel;
+const int minBatteryLevel; 
+bool isInConfiguration = false; 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(5, 6, 8, 9, 10, 11); 
-
+/*
+batteryControl lo que encenderá o apagará el led indicando baja batería
+*/
 void setup() {
   DDRD = (1 << tempControl) | (1 << humControl);
   DDRB = (1 << batteryControl);
   PORTB &= ~(1 << batteryControl);
-  PORTD = (1 << PD0); //activo la resistencia pullUp en el pin 0
+  PORTD = (1 << PD0) | (1 << tempControl);//activo la resistencia pullUp en el pin 0
   dht.begin();
   Serial.begin(9600);
   lcd.begin(16, 2);
 
-  EIMSK = (1 << INT0) | (1 << INT1); //activo la interrupción del PD2
-  EICRA = (1 << ISC01) | (1 << ISC11); // flanco de bajada 
+  EIMSK = (1 << INT0) | (1 << INT1);//activo la interrupción del PD2 y PD3
+  EICRA = (1 << ISC01) | (1 << ISC11);//flanco de bajada 
   sei();
 }
 /*
  set cursor (col, row) 
 */
 void loop() {
-  
-  //Serial.println(t);
+  // control de los estados de menú
+if(menuState == 0){
+  isInConfiguration = false;
+}
+if((PIND = (1 << PD0)) == 0){
+  menuState += 1;
+  isInConfiguration = true;
+  _delay_ms(200);
+  if(menuState > 4){
+    menuState = 0;
+    isInConfiguration = false; 
+  } 
+}
+// fin de control de menú
 
+//Código que maneja el control de variables en el horno
   if(t >= maxTemp){
      PORTD &= ~(1 << tempControl);
   }else if (t < minTemp){
@@ -57,19 +72,26 @@ void loop() {
   }else{
     PORTB &= ~(1 << batteryControl);
   }
- 
+ //Fin de código de control de variables en el horno
 
   unsigned long now = millis();
   if(now - lastTime >= interval){
     lastTime = now;
     t = dht.readTemperature();
     h = dht.readHumidity();
-    UpdateText();
+    if(!isInConfiguration){
+      PrincipalMenuText();
+    }   
   }
+
+  if(isInConfiguration){
+    ConfigurationMenuText();
+  }
+ 
    
 }
 
-void UpdateText(){
+void PrincipalMenuText(){
   lcd.setCursor(0,0);
   lcd.print("temp=");
   lcd.print(t);
@@ -82,12 +104,72 @@ void UpdateText(){
   
 }
 
+void ConfigurationMenuText(){
+  if(menuState == 1){
+     lcd.setCursor(0,0);
+     lcd.print("MaxTemp= ");
+     lcd.print(maxTemp);
+  }else if(menuState == 2){
+     lcd.setCursor(0,0);
+     lcd.print("MinTemp= ");
+     lcd.print(minTemp);
+  }else if(menuState == 3){
+     lcd.setCursor(0,0);
+     lcd.print("MaxHum= ");
+     lcd.print(maxHum);
+  }else if(menuState == 4){
+     lcd.setCursor(0,0);
+     lcd.print("MinHum= ");
+     lcd.print(minHum);
+  }
+  
+  
+}
+/*
+if(maxTemp <= minTemp){
+      maxTemp = minTemp + 2;
+    }
+*/
+
+//Incremento de las variables
 ISR(INT0_vect) {
-  Serial.println("hon");
+   if(menuState == 1){
+    maxTemp++; 
+  }else if(menuState == 2){
+    minTemp++;
+    if(minTemp >= maxTemp){
+      minTemp = maxTemp - 2;
+    }
+  }else if(menuState == 3){
+    maxHum++;
+  }else if(menuState == 4){
+    minHum++;
+    if(minHum >= maxHum){
+      minHum = maxHum - 2;
+    }
+  }
+  _delay_ms(10);
 }
 
+//Decremento de las variables
 ISR(INT1_vect) {
-  Serial.println("hon1");
+ if(menuState == 1){
+    maxTemp--; 
+    if(maxTemp <= minTemp){
+      maxTemp = minTemp + 2;
+    }  
+  }else if(menuState == 2){
+    minTemp--;      
+  }else if(menuState == 3){
+    maxHum--;
+    if(maxHum <= minHum){
+      maxHum = minHum + 2;
+    }  
+  }else if(menuState == 4){
+    minHum--;
+    
+  }
+  _delay_ms(10);
 }
 
 
